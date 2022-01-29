be01ef8bdbfbe07fb8decf641e36243e256ceb7d
Munir Jahangir <munir.jahangir@gmail.com>
2022-01-29 15:13:55 +0000

    reflected new code in lib file

--- mjproj/lib.py
+++ mjproj/lib.py
@@ -7,8 +7,13 @@ def try_me():
     demo_date = date(2022, 3, 19)
     demo_day = datetime.combine(demo_date, demo_time) + timedelta(hours=10)
 
-    days_to_demo = (demo_day - now) / timedelta(days=1)
+    days = (demo_day - now) / timedelta(days=1)
+    hours = ((demo_day - now) % timedelta(days=1)) / timedelta(hours=1)
+    mins = ((demo_day - now) % timedelta(hours=1)) / timedelta(minutes=1)
+    secs = ((demo_day - now) % timedelta(minutes=1)) / timedelta(seconds=1)
 
-    print(f'It is {days_to_demo:.0f} days to demo day')
+    print(
+        f'It is {days:.0f} days, {hours:.0f} hours, {mins:.0f} minutes and {secs:.0f} seconds to demo day...'
+    )
 
 try_me()
